4e521618dad34b7146310d731a1511edf7f128fb
Abrham007 <abrhamaraya111@gmail.com>
2024-03-12 18:38:07 +0300

    finished the Header components with responsivness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,28 @@
 import NavBar from "./Navbar";
-import cartIcon from "../../../public/cart/card-icon.svg";
+import cartIcon from "../../../public/shared/desktop/icon-cart.svg";
+import logoIcon from "../../../public/shared/desktop/logo.svg";
+import Image from "next/image";
+import LinkList from "../UI/Link/LinkList";
 
 export default function Header() {
   return (
-    <header className="w-full h-[90px] px-6 bg-Black flex justify-between items-center ">
-      <NavBar></NavBar>
-      <h1 className="text-xl2 tracking-tight text-White">audiophile</h1>
-      <button>
-        <img src="./cart/cart-icon.svg" alt="cart icon" />
-      </button>
+    <header className="bg-Black md:px-10">
+      <div className="w-full max-w-[1110px] h-[90px] px-6 md:px-0 lg:px-0 lg:py-9 lg:mx-auto  flex md:gap-[42px] lg:gap-[197px] justify-between items-center border-b-[1px] border-b-solid border-b-White">
+        <NavBar></NavBar>
+
+        <Image
+          src={logoIcon}
+          alt="audiophile"
+          className="text-xl2 tracking-tight text-White"
+        ></Image>
+
+        <nav className="hidden lg:flex">
+          <LinkList className="flex gap-[34px]"></LinkList>
+        </nav>
+        <button className="md:ml-auto">
+          <Image src={cartIcon} alt="cart icon"></Image>
+        </button>
+      </div>
     </header>
   );
 }
