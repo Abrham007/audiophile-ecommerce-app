a11b724e6408f74549da0df761694599a72393bc
Abrham007 <abrhamaraya111@gmail.com>
2024-03-12 18:38:35 +0300

    finihsed the reusabel Button component

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,25 +1,26 @@
 type ButtonProps = {
   $type: string;
   children: any;
+  props: any;
 };
 
-export default function Button({ $type, children }: ButtonProps) {
+export default function Button({ $type, children, ...props }: ButtonProps) {
   switch ($type) {
     case "1":
       return (
-        <button className="px-[30px] py-[15px] bg-Orange hover:bg-LightOrange text-sm text-White font-bold border-none outline-none">
+        <button
+          {...props}
+          className="px-[30px] py-[15px] bg-Orange hover:bg-LightOrange text-sm text-White font-bold border-none outline-none"
+        >
           {children}
         </button>
       );
     case "2":
       return (
-        <button className="px-[30px] py-[15px] bg-White hover:bg-Black text-sm text-Black hover:text-White font-bold border-[1px] border-solid border-Black outline-none">
-          {children}
-        </button>
-      );
-    case "3":
-      return (
-        <button className="flex gap-[13px] items-center justify-center bg-transparent text-sm text-[#00000050] hover:text-Orange font-bold border-none outline-none">
+        <button
+          {...props}
+          className="px-[30px] py-[15px] bg-White hover:bg-Black text-sm text-Black hover:text-White font-bold border-[1px] border-solid border-Black outline-none"
+        >
           {children}
         </button>
       );
