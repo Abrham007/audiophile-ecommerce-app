c9a550ede205f9e23f8974c148f57e77930efe29
Abrham007 <abrhamaraya111@gmail.com>
2024-03-13 21:52:56 +0300

    fixed a sytle bug

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import LinkList from "../UI/Link/LinkList";
 
 export default function Header() {
   return (
-    <header className="bg-Black md:px-10">
+    <header className="fixed top-0 left-0 bg-Black md:px-10">
       <div className="w-full max-w-[1110px] h-[90px] px-6 md:px-0 lg:px-0 lg:py-9 lg:mx-auto  flex md:gap-[42px] lg:gap-[197px] justify-between items-center border-b-[1px] border-b-solid border-b-White">
         <NavBar></NavBar>
 
