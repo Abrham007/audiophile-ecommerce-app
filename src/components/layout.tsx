43afdfc6048e1e7c7e15d6505e55a704164aecf8
Abrham007 <abrhamaraya111@gmail.com>
2024-03-14 18:40:33 +0300

    corrected the font bug

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,11 @@
-import { Manrope } from "next/font/google";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
-const manrope = Manrope({ subsets: ["latin"], variable: "--font-manrope" });
 
 export default function Layout({ children }: any) {
   return (
     <>
       <Header></Header>
-      <main className={`${manrope.variable}`}>{children}</main>
+      <main>{children}</main>
       <Footer></Footer>
     </>
   );
