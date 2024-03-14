3ed2840e3e8d0f2d3fe0b0cda38ef96c3de34471
Abrham007 <abrhamaraya111@gmail.com>
2024-03-14 10:22:53 +0300

    finished the header of the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,17 @@ import Button from "@/components/UI/Button/Button";
 export default function Home() {
   return (
     <div className="flex flex-col gap-10">
-      <header className="h-[600px] md:h-[730px] flex justify-center items-center bg-LightBlack bg-[url('../../public/home/mobile/image-header.jpg')] md:bg-[url('../../public/home/tablet/image-header.jpg')] bg-bottom md:bg-center bg-cover bg-no-repeat">
-        <div className="w-[315px] md:w-min flex flex-col items-center gap-6">
-          <h1 className="flex flex-col gap-4">
-            <span className="text-sm text-White text-center uppercase opacity-50">
+      <header className="h-[600px] md:h-[730px] flex justify-center items-center bg-Black bg-[url('../../public/home/mobile/image-header.jpg')] md:bg-[url('../../public/home/tablet/image-header.jpg')] lg:bg-[url('../../public/home/desktop/image-hero.jpg')] bg-bottom md:bg-center bg-cover bg-no-repeat">
+        <div className="w-[315px] md:w-min lg:w-[1110px] flex flex-col items-center lg:items-start gap-6 text-center lg:text-left">
+          <h1 className="lg:w-[395px] flex flex-col gap-4 md:gap-6">
+            <span className="text-sm text-White  uppercase opacity-50">
               New Product
             </span>
-            <span className="text-[2.25rem] md:text-xl5 text-White text-center leading-[2.5rem] tracking-[0.08038rem] uppercase font-bold">
+            <span className="text-[2.25rem] md:text-xl5 text-White leading-[2.5rem] tracking-[0.08038rem] uppercase font-bold">
               XX99 Mark II HeadphoneS
             </span>
           </h1>
-          <p className="text-base text-White text-center opacity-75">
+          <p className="lg:w-[350px] text-base text-White opacity-75 lg:mb-4">
             Experience natural, lifelike audio and exceptional build quality
             made for the passionate music enthusiast.
           </p>
