06676d822ea64100656dbf0f927af8250a95abb5
Abrham007 <abrhamaraya111@gmail.com>
2024-03-15 13:32:27 +0300

    corrected a style bug

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ import LinkList from "../UI/Link/LinkList";
 export default function Header() {
   return (
     <header className="w-full max-w-[1110px] h-[90px] absolute top-0 left-0 lg:left-[50%] lg:-translate-x-[50%] md:px-10 lg:px-0">
-      <div className="w-full max-w-[1110px] h-[90px] px-6 md:px-0 lg:px-0 lg:py-9 lg:mx-auto  flex md:gap-[42px] lg:gap-[197px] justify-between items-center border-b-[1px] border-b-solid border-b-White">
+      <div className="w-full max-w-[1110px] h-[90px] px-6 md:px-0 lg:px-0 lg:py-9 lg:mx-auto  flex md:gap-[42px] lg:gap-[197px] justify-between items-center border-b-[1px] border-b-solid border-b-[#FFFFFF50]">
         <HeaderNav></HeaderNav>
 
         <Image
