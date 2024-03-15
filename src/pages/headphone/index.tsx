189f50afdc32a5f6f99561edd9599aa7d1934505
Abrham007 <abrhamaraya111@gmail.com>
2024-03-15 13:34:34 +0300

    made the componenet more leaner

--- src/pages/headphone/index.tsx
+++ src/pages/headphone/index.tsx
@@ -1,7 +1,18 @@
+import Navbar from "@/components/Navbar/Navbar";
+import ProductHeader from "@/components/ProductPage/ProductHeader";
+import ProductList from "@/components/ProductPage/ProductList";
+
+import Summary from "@/components/Summary";
+
 export default function Headphone() {
   return (
-    <div>
-      <h1>headphone</h1>
+    <div className="flex flex-col">
+      <ProductHeader title="headphones"></ProductHeader>
+      <section className="flex flex-col gap-[120px] px-6 md:px-10 lg:px-0 mt-[64px] mb-[120px] md:my-[120px] lg:my-[160px] ">
+        <ProductList></ProductList>
+        <Navbar></Navbar>
+        <Summary></Summary>
+      </section>
     </div>
   );
 }
