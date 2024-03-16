c8cd386b50e025d4039e0ee856a5d22e75e35797
Abrham007 <abrhamaraya111@gmail.com>
2024-03-16 14:50:29 +0300

    corrected a style bug

--- src/components/UI/Inputs/NumberInput.tsx
+++ src/components/UI/Inputs/NumberInput.tsx
@@ -9,19 +9,19 @@ export default function NumberInput({
   decrement,
 }: NumberInputProps) {
   return (
-    <div className="w-30 h-12 bg-VeryDarkWhite flex gap-5 items-center justify-center">
+    <div className="w-[120px] h-[48px] bg-VeryDarkWhite flex gap-5 items-center justify-center">
       <button
         onClick={() => decrement(number - 1)}
-        className="text-xs text-[#00000025] hover:text-Orange leading-normal tracking-[0625rem]"
+        className="text-xs text-[#00000025] hover:text-Orange leading-normal tracking-[0.0625rem]"
       >
         -
       </button>
-      <span className="text-xs text-Black  leading-normal tracking-[0625rem]">
+      <span className="text-xs text-Black  leading-normal tracking-[0.0625rem]">
         {number}
       </span>
       <button
         onClick={() => increment(number + 1)}
-        className="text-xs text-[#00000025] hover:text-Orange leading-normal tracking-[0625rem]"
+        className="text-xs text-[#00000025] hover:text-Orange leading-normal tracking-[0.0625rem]"
       >
         +
       </button>
