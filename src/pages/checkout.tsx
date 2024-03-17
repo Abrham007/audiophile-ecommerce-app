a3e31d5059a08e58d2556b96c048c55bad5b13fe
Abrham007 <abrhamaraya111@gmail.com>
2024-03-17 12:33:55 +0300

    added the summary section

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,12 +1,13 @@
 import CheckoutForm from "@/components/CheckoutPage/CheckoutForm";
+import CheckoutSummary from "@/components/CheckoutPage/CheckoutSummary";
 
 export default function Checkout() {
   return (
-    <div className="w-full max-w-[1110px] mx-auto flex flex-col gap-6 px-6 md:px-10 lg:px-0 mt-[106px] md:mt-[122px] lg:mt-[170px] mb-[120px] bg-DarkWhite">
-      <button className="self-start">Go Back</button>
-
-      <section className="flex flex-col gap-8">
+    <div className="px-6 md:px-10 lg:px-0 pt-[106px] md:pt-[122px] lg:pt-[170px] pb-[120px] bg-DarkWhite">
+      <section className="w-full max-w-[1110px] mx-auto flex flex-col gap-8">
+        <button className="self-start -mb-2">Go Back</button>
         <CheckoutForm></CheckoutForm>
+        <CheckoutSummary></CheckoutSummary>
       </section>
     </div>
   );
