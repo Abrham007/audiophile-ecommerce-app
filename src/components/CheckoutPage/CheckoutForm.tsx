43eef0e008ebaeb72aa0513d5f87d1fbeeead14f
Abrham007 <abrhamaraya111@gmail.com>
2024-03-17 12:32:45 +0300

    finished the styles checkout form

--- src/components/CheckoutPage/CheckoutForm.tsx
+++ src/components/CheckoutPage/CheckoutForm.tsx
@@ -3,7 +3,7 @@ import TextInput from "../UI/Inputs/TextInput";
 
 export default function CheckoutForm() {
   return (
-    <div className="p-6 flex flex-col gap-8 bg-White">
+    <div className="lg:w-[730px] p-6 md:px-[28px] lg:py-[30px] flex flex-col gap-8 bg-White rounded-lg">
       <h1 className="text-xl2 md:text-xl3 text-Black tracking-[0.0625rem] uppercase">
         CHECKOUT
       </h1>
@@ -35,11 +35,14 @@ export default function CheckoutForm() {
             shipping info
           </legend>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-6">
-            <TextInput
-              name="address"
-              label="Your Address"
-              placeholder="1137 Williams Avenue"
-            ></TextInput>
+            <div className="md:col-span-2">
+              <TextInput
+                name="address"
+                label="Your Address"
+                placeholder="1137 Williams Avenue"
+              ></TextInput>
+            </div>
+
             <TextInput
               name="zip-code"
               label="ZIP Code"
@@ -61,11 +64,14 @@ export default function CheckoutForm() {
           <legend className="text-xs text-Orange uppercase mb-4">
             payment details
           </legend>
-          <fieldset>
-            <legend className="text-Black text-[0.75rem] -tracking-[0.01338rem] font-bold mb-4">
-              Payment Method
-            </legend>
-            <div className="flex flex-col gap-4">
+          <fieldset className="flex flex-col md:flex-row md:justify-between gap-4">
+            <div className="flex-1">
+              <legend className="text-Black text-[0.75rem] -tracking-[0.01338rem] font-bold ">
+                Payment Method
+              </legend>
+            </div>
+
+            <div className="flex flex-col gap-4 flex-1">
               <RadioInput name="payment-method" label="e-Money"></RadioInput>
               <RadioInput
                 name="payment-method"
@@ -73,17 +79,21 @@ export default function CheckoutForm() {
               ></RadioInput>
             </div>
           </fieldset>
-          <div className="flex flex-col gap-6">
-            <TextInput
-              name="e-money-number"
-              label="e-Money Number"
-              placeholder="238521993"
-            ></TextInput>
-            <TextInput
-              name="e-money-pin"
-              label="e-Money PIN"
-              placeholder="6891"
-            ></TextInput>
+          <div className="flex flex-col md:flex-row gap-6 md:gap-4">
+            <div className="flex-1">
+              <TextInput
+                name="e-money-number"
+                label="e-Money Number"
+                placeholder="238521993"
+              ></TextInput>
+            </div>
+            <div className="flex-1">
+              <TextInput
+                name="e-money-pin"
+                label="e-Money PIN"
+                placeholder="6891"
+              ></TextInput>
+            </div>
           </div>
         </fieldset>
       </form>
