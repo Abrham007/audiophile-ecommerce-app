1964ffcc8d913c60f2a2041310a04ab40963fc5d
Abrham007 <abrhamaraya111@gmail.com>
2024-03-19 17:16:36 +0300

    made the component more reusable

--- src/components/UI/Inputs/NumberInput.tsx
+++ src/components/UI/Inputs/NumberInput.tsx
@@ -1,5 +1,6 @@
 type NumberInputProps = {
   number: number;
+  className?: string;
   increment: (num: number) => void;
   decrement: (num: number) => void;
 };
@@ -7,9 +8,12 @@ export default function NumberInput({
   number,
   increment,
   decrement,
+  className,
 }: NumberInputProps) {
   return (
-    <div className="w-[120px] h-[48px] bg-VeryDarkWhite flex gap-5 items-center justify-center">
+    <div
+      className={`w-full h-full bg-VeryDarkWhite flex gap-5 items-center justify-center ${className}`}
+    >
       <button
         onClick={() => decrement(number - 1)}
         className="text-xs text-[#00000025] hover:text-Orange leading-normal tracking-[0.0625rem]"
