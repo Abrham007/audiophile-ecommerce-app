9b1449ea4ede1b30c6fb3d385c981954d2eb129b
Abrham007 <abrhamaraya111@gmail.com>
2024-03-21 14:45:35 +0300

    added the cart context

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,15 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import CartContext, { CartContextProvider } from "@/store/CartContext";
+import { useContext } from "react";
+import Main from "./Main";
 
 export default function Layout({ children }: any) {
   return (
-    <>
+    <CartContextProvider>
       <Header></Header>
-      <main>{children}</main>
+      <Main>{children}</Main>
       <Footer></Footer>
-    </>
+    </CartContextProvider>
   );
 }
