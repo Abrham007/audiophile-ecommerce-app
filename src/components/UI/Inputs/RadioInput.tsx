47313b701e51c780930358fb0775c5c7cf866dc8
Abrham007 <abrhamaraya111@gmail.com>
2024-03-22 18:16:30 +0300

    made the component functional

--- src/components/UI/Inputs/RadioInput.tsx
+++ src/components/UI/Inputs/RadioInput.tsx
@@ -1,17 +1,29 @@
 type RadioInputProps = {
   name: string;
   label: string;
+  checked?: boolean;
+  register: any;
+  value: string;
 };
 
-export default function RadioInput({ name, label }: RadioInputProps) {
+export default function RadioInput({
+  name,
+  label,
+  checked = false,
+  register,
+  value,
+}: RadioInputProps) {
   return (
     <label className="py-[18px] pl-4 flex gap-4 hover:cursor-pointer border border-solid border-[#CFCFCF] rounded-lg hover:border-Orange focus:border-Orange">
       <input
         name={name}
+        {...register(`${name}`)}
+        value={value}
         type="radio"
-        className="peer absolute opacity-0 h-0 w-0"
+        className="peer absolute w-0 h-0 opacity-0"
+        defaultChecked={checked}
       />
-      <span className="w-5 h-5 relative bg:transparent border border-solid border-[#CFCFCF] rounded-full after:w-[10] after:h-[10px] after:rounded-full after:bg-Orange after:absolute after:top-[5px] after:left-[5px] after:hidden peer-checked:block"></span>
+      <span className="w-5 h-5 relative bg:transparent border border-solid border-[#CFCFCF] rounded-full  after:w-[10px] after:h-[10px]  after:rounded-full after:bg-Orange after:absolute after:top-[50%] after:left-[50%] after:-translate-y-[50%] after:-translate-x-[50%] after:hidden peer-checked:after:block"></span>
       <span className="text-sm -tracking-[0.01563rem] font-bold">{label}</span>
     </label>
   );
