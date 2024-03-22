4f30b17c2d4a5d22e75ee19653942b313618ab90
Abrham007 <abrhamaraya111@gmail.com>
2024-03-22 18:15:27 +0300

    made the component interactive and dynamic

--- src/components/CheckoutPage/CheckoutForm.tsx
+++ src/components/CheckoutPage/CheckoutForm.tsx
@@ -1,13 +1,88 @@
+import Image from "next/image";
 import RadioInput from "../UI/Inputs/RadioInput";
 import TextInput from "../UI/Inputs/TextInput";
+import CashIcon from "../../../public/checkout/icon-cash-on-delivery.svg";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  phoneNumber: number;
+  address: string;
+  zipCode: number;
+  city: string;
+  country: string;
+  paymentMethod: string;
+  eMoneyNumber: string;
+  eMoneyPin: string;
+};
 
 export default function CheckoutForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
+  let paymentMethodDispaly = (
+    <div className="flex flex-col md:flex-row gap-6 md:gap-4">
+      <div className="flex-1">
+        <TextInput
+          name="eMoneyNumber"
+          register={register}
+          errors={errors}
+          label="e-Money Number"
+          placeholder="238521993"
+        ></TextInput>
+      </div>
+      <div className="flex-1">
+        <TextInput
+          name="eMoneyPin"
+          register={register}
+          errors={errors}
+          label="e-Money PIN"
+          placeholder="6891"
+        ></TextInput>
+      </div>
+    </div>
+  );
+
+  if (watch("paymentMethod") === "cash-on-delivery") {
+    paymentMethodDispaly = (
+      <div className="flex gap-6 md:gap-8 flex-col md:flex-row items-center md:items-start">
+        <Image
+          src={CashIcon}
+          alt=""
+          width={48}
+          height={48}
+          className="shrink-0"
+        ></Image>
+        <p className="text-base text-Black opacity-50">
+          The ‘Cash on Delivery’ option enables you to pay in cash when our
+          delivery courier arrives at your residence. Just make sure your
+          address is correct so that your order will not be cancelled.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="lg:w-[730px] p-6 md:px-[28px] lg:py-[30px] flex flex-col gap-8 bg-White rounded-lg">
       <h1 className="text-xl2 md:text-xl3 text-Black tracking-[0.0625rem] uppercase">
         CHECKOUT
       </h1>
-      <form className="flex flex-col gap-8">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-8"
+        id="checkout-form"
+        noValidate
+      >
         <fieldset>
           <legend className="text-xs text-Orange uppercase mb-4">
             Billing details
@@ -15,16 +90,23 @@ export default function CheckoutForm() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-6">
             <TextInput
               name="name"
+              register={register}
+              errors={errors}
               label="Name"
               placeholder="Alexei Ward"
             ></TextInput>
             <TextInput
               name="email"
+              register={register}
+              errors={errors}
+              type="email"
               label="Email Address"
               placeholder="alexei@mail.com"
             ></TextInput>
             <TextInput
-              name="phone-number"
+              name="phoneNumber"
+              register={register}
+              errors={errors}
               label="Phone Number"
               placeholder="Alexei Ward"
             ></TextInput>
@@ -38,23 +120,31 @@ export default function CheckoutForm() {
             <div className="md:col-span-2">
               <TextInput
                 name="address"
+                register={register}
+                errors={errors}
                 label="Your Address"
                 placeholder="1137 Williams Avenue"
               ></TextInput>
             </div>
 
             <TextInput
-              name="zip-code"
+              name="zipCode"
+              register={register}
+              errors={errors}
               label="ZIP Code"
               placeholder="ZIP Code"
             ></TextInput>
             <TextInput
               name="city"
+              register={register}
+              errors={errors}
               label="City"
               placeholder="New York"
             ></TextInput>
             <TextInput
               name="country"
+              register={register}
+              errors={errors}
               label="Country"
               placeholder="Country"
             ></TextInput>
@@ -72,29 +162,22 @@ export default function CheckoutForm() {
             </div>
 
             <div className="flex flex-col gap-4 flex-1">
-              <RadioInput name="payment-method" label="e-Money"></RadioInput>
               <RadioInput
-                name="payment-method"
+                name="paymentMethod"
+                register={register}
+                value="e-Money"
+                label="e-Money"
+                checked={true}
+              ></RadioInput>
+              <RadioInput
+                name="paymentMethod"
+                register={register}
+                value="cash-on-delivery"
                 label="Cash on Delivery"
               ></RadioInput>
             </div>
           </fieldset>
-          <div className="flex flex-col md:flex-row gap-6 md:gap-4">
-            <div className="flex-1">
-              <TextInput
-                name="e-money-number"
-                label="e-Money Number"
-                placeholder="238521993"
-              ></TextInput>
-            </div>
-            <div className="flex-1">
-              <TextInput
-                name="e-money-pin"
-                label="e-Money PIN"
-                placeholder="6891"
-              ></TextInput>
-            </div>
-          </div>
+          {paymentMethodDispaly}
         </fieldset>
       </form>
     </div>
