c961b2a2e35fc5030af5497f769c9cce00a01150
Abrham007 <abrhamaraya111@gmail.com>
2024-03-22 18:50:27 +0300

    finished the checkout page

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,9 +1,21 @@
 import CheckoutForm from "@/components/CheckoutPage/CheckoutForm";
 import CheckoutSummary from "@/components/CheckoutPage/CheckoutSummary";
 import { useRouter } from "next/router";
+import Modal from "../components/Modal";
+import SuccessMessage from "../components/SuccessMessage";
+import { useState } from "react";
 
 export default function Checkout() {
   const router = useRouter();
+  const [isFinished, setIsFinished] = useState(false);
+
+  function handleOpenSuccessMessage() {
+    setIsFinished(true);
+  }
+
+  function handleCloseSuccessMessage() {
+    setIsFinished(false);
+  }
   return (
     <>
       <div className="px-6 md:px-10 lg:px-0 pt-[106px] md:pt-[122px] lg:pt-[170px] pb-[120px] bg-DarkWhite">
@@ -17,9 +29,19 @@ export default function Checkout() {
         </div>
 
         <section className="w-full max-w-[1110px] mx-auto flex flex-col lg:flex-row gap-8 lg:gap-[30px] lg:items-start">
-          <CheckoutForm></CheckoutForm>
+          <CheckoutForm openSuccess={handleOpenSuccessMessage}></CheckoutForm>
           <CheckoutSummary></CheckoutSummary>
         </section>
+
+        {isFinished && (
+          <Modal
+            isOpen={isFinished}
+            setIsOpen={handleCloseSuccessMessage}
+            className="rounded-lg"
+          >
+            <SuccessMessage></SuccessMessage>
+          </Modal>
+        )}
       </div>
     </>
   );
