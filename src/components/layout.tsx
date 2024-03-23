60d2f79e321db30b0250838025b1443e70598ee0
Abrham007 <abrhamaraya111@gmail.com>
2024-03-23 16:13:18 +0300

    improved the SEO

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,13 +3,24 @@ import Footer from "./Footer/Footer";
 import CartContext, { CartContextProvider } from "@/store/CartContext";
 import { useContext } from "react";
 import Main from "./Main/Main";
+import Head from "next/head";
 
 export default function Layout({ children }: any) {
   return (
-    <CartContextProvider>
-      <Header></Header>
-      <Main>{children}</Main>
-      <Footer></Footer>
-    </CartContextProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico"></link>
+        <title>Audiophile E-Commerce App</title>
+        <meta
+          name="decription"
+          content="This one of the project's of Abrham Araya made by Next, React and TypeScript hosted at Heroku"
+        />
+      </Head>
+      <CartContextProvider>
+        <Header></Header>
+        <Main>{children}</Main>
+        <Footer></Footer>
+      </CartContextProvider>
+    </>
   );
 }
