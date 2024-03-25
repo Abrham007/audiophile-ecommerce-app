076dd132d1f16aa56500ba5df44b5020899ef6ba
Abrham007 <abrhamaraya111@gmail.com>
2024-03-25 17:50:02 +0300

    corrected the case sensetive bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "@/components/Layout";
+import Layout from "@/components/layout";
 import { useRouter } from "next/router";
 import { Manrope } from "next/font/google";
 
