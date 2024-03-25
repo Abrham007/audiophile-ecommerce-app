a003cefe3be3656879d071437ed45a8fa997da89
Abrham007 <abrhamaraya111@gmail.com>
2024-03-25 16:01:21 +0300

    improved the UX

--- src/components/Header/NavDropdown.tsx
+++ src/components/Header/NavDropdown.tsx
@@ -8,6 +8,9 @@ type NavDropdownProps = {
 export default function NavDropdown({ isOpen, setIsOpen }: NavDropdownProps) {
   return (
     <div
+      onClick={() => {
+        setIsOpen(false);
+      }}
       className={`absolute top-[90px] left-0 right-0 bg-White rounded-b-lg z-10 before:w-full before:h-full ${
         isOpen ? "block" : "hidden"
       } before:opacity-50 before:fixed before:top-[90px] before:left-0 before:bottom-0 before:right-0 before:bg-Black lg:hidden before:z-9`}
